9edb699b12dfccaa362fd938a4da4807676e0178
mschoeffel <michael.schoeffel@t-online.de>
2019-03-11 18:37:02 +0100

    Spring Form Regex

--- out/artifacts/MVC/WEB-INF/view/springFormValidation.jsp
+++ out/artifacts/MVC/WEB-INF/view/springFormValidation.jsp
@@ -28,6 +28,11 @@
 
     <br><br>
 
+    PostalCode (*): <form:input path="postalCode" />
+    <form:errors path="postalCode" cssClass="error" />
+
+    <br><br>
+
     Tickets (*): <form:input path="tickets" />
     <form:errors path="tickets" cssClass="error" />
 
--- src/spring/MVC/Customer.java
+++ src/spring/MVC/Customer.java
@@ -10,6 +10,10 @@ public class Customer {
     @Size(min=1, message="is required")
     private String lastName;
 
+    @Pattern(regexp = "^[0-9]{5}", message = "only 5 digits allowed in german postal codes")
+    @NotNull(message="is required")
+    private String postalCode;
+
     @Min(value=0, message="must be grater then or equal to zero")
     @Max(value=10, message="musst be less or euqal than 10")
     private int tickets;
@@ -37,5 +41,13 @@ public class Customer {
 
     public void setTickets(int tickets) {
         this.tickets = tickets;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 }
